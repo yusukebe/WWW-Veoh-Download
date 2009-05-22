ebda9a2891b2b0d23fb7f953d95805f24ccd8119
Yusuke Wada <yusukebe@cpan.org>
2009-05-22 17:19:16 +0900

    fixed param

--- sample/fetch-video.pl
+++ sample/fetch-video.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use WWW::Veoh::Download;
 
-my ( $api_key, $video_id ) = @_;
+my ( $api_key, $video_id ) = @ARGV;
 my $client = WWW::Veoh::Download->new( api_key => $api_key );
 $client->download( $video_id , 'sample.mp4' );
 
